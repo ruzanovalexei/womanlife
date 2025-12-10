f6026e3f02fc9c6984fd013751f99455b1758367
ruzanovalexei <ruzanov.alexei@gmail.com>
2025-12-10 18:28:12 +0400

    Исправлен расчет фертильных дней. Подготовка релиза 1.0.2

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -27,7 +27,7 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
+        versionCode = 100002
         versionName = flutter.versionName
         
         // ✅ ДОБАВЛЯЕМ: App ID Яндекс Рекламы
