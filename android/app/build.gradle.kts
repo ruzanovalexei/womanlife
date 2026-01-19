da0820c39e2a6277d95d4bf1dca339cb3d850710
ruzanovalexei <ruzanov.alexei@gmail.com>
2026-01-19 21:10:45 +0400

    Релиз 1.0.8

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -27,7 +27,7 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = 100007
+        versionCode = 100008
         versionName = flutter.versionName
         
         // ✅ ДОБАВЛЯЕМ: App ID Яндекс Рекламы
